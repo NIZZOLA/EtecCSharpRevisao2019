7e34c49a6217eedfad485a413fb31f594d45000b
Controle <Controle@PCPROF-LAB01>
2019-04-17 22:25:15 -0300

    Implementação de funcionário e métodos

---
+++ Aplicacao1/Data/FuncionarioRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao1.Data
+{
+    public class FuncionarioRepository
+    {
+        private AppEstoqueContext ctx;
+
+        public FuncionarioRepository()
+        {
+            ctx = new AppEstoqueContext();
+        }
+        
+        public void Incluir( Funcionario fun )
+        {
+            ctx.Funcionarios.Add(fun);
+            ctx.SaveChanges();
+        }
+
+        public void Alterar( Funcionario fun )
+        {
+            Funcionario funcad = this.Consultar( fun.FuncionarioId);
+
+        }
+
+        public void Excluir( int codigo )
+        {
+            
+        }
+
+        public Funcionario Consultar( int codigo )
+        {
+            Funcionario fun = ctx.Funcionarios.Where(a => a.FuncionarioId == codigo).FirstOrDefault();
+            return fun;
+        }
+    }
+}
---
+++ Aplicacao1/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aplicacao1
+{
+    public class Funcionario
+    {
+        [Key]
+        public int FuncionarioId { get; set; }
+        [MaxLength(50)]
+        public string Nome { get; set; }
+        [MaxLength(50)]
+        public string Cargo { get; set; }
+        [MaxLength(15)]
+        public string Fone { get; set; }
+        [MaxLength(80)]
+        public string Email { get; set; }
+        [MaxLength(10)]
+        public string Senha { get; set; }
+        public DateTime Nascimento { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
